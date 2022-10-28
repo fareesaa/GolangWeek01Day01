3e6b95c58ce63ce927e5695581068db0aac19e04
fareesaa <97422732+fareesaa@users.noreply.github.com>
2022-10-28 09:42:19 +0700

    Add files via upload

---
+++ Assignment2.go
@@ -0,0 +1,10 @@
+package Assignment2
+
+import "fmt"
+
+func main() {
+	fmt.Println("Nama			: Ahmad Farisi")
+	fmt.Println("Tempat Tanggal Lahir 	: Jakarta, 13 Oktober 2001")
+	fmt.Println("Jenis Kelamin	 	: Laki-laki")
+	fmt.Println("Pekerjaan			: Graphic Designer")
+}
